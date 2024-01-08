5985955c12dbf2f38991b23513b0b0fff33a33e0
김주령 <juryeong@kimjuryeong.local>
2024-01-08 21:54:57 +0900

    혼공컴운 ch01 정리

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,13 @@
-var store = []
+var store = [{
+        "title": "[혼공자바] 1주차 과제",
+        "excerpt":"1. JDK 설치화면 인증하기 (기본미션) 2. p.88 확인문제 9번 (선택미션) 1. public class Main { public static void main(String[] args) { long var1 = 2L; float var2 = 1.8f; double var3 = 2.5; String var4 = \"3.9\"; int result = (int)var1 + (int)var2 + (int)(var3 + Float.parseFloat(var4)); System.out.println(result); } }...","categories": ["혼공자바"],
+        "tags": ["Java","혼공자바","혼공자바 연습문제","혼공학습단 11기"],
+        "url": "/%ED%98%BC%EA%B3%B5%EC%9E%90%EB%B0%94/1.week1/",
+        "teaser": null
+      },{
+        "title": "[혼공컴운] 1주차 미션",
+        "excerpt":"1. P.51, 65 확인 문제 3번 풀고 인증하기(기본 미션) 프로그램이 실행되려면 반드시 (메모리)에 저장되어 있어야 합니다. 1101 ➡️ 0010 ➡️ 0011 1101(2)을 음수로 표현한 값은 0011입니다. 2. P100. 스택과 큐의 개념을 정리하기(선택미션) 1. 스택 한쪽 끝이 막혀 있는 저장 공간 LIFO : 후입선출 방식 PUSH : 스택에 새로운 데이터를 저장하는...","categories": ["혼공컴운"],
+        "tags": ["컴퓨터구조","운영체제","혼공컴운 연습문제","혼공학습단 11기"],
+        "url": "/%ED%98%BC%EA%B3%B5%EC%BB%B4%EC%9A%B4/1.week1-CO/",
+        "teaser": null
+      }]
